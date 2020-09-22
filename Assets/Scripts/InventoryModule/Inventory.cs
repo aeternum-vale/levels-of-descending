799e16edc22a5493478e696d897179208cd7967c
Artur Dedyulya <aeternum-vale@ya.ru>
2020-09-22 12:33:57 +0300

    fix insulating tape grabbing

--- Assets/Scripts/InventoryModule/Inventory.cs
+++ Assets/Scripts/InventoryModule/Inventory.cs
@@ -24,7 +24,7 @@ namespace InventoryModule
                 [EInventoryItemId.SCALPEL] = new InventoryItemData(),
                 [EInventoryItemId.E_PANEL_KEY] = new InventoryItemData(),
                 [EInventoryItemId.SCREWDRIVER] = new InventoryItemData {IsInStock = true},
-                [EInventoryItemId.INSULATING_TAPE] = new InventoryItemData {IsInStock = true},
+                [EInventoryItemId.INSULATING_TAPE] = new InventoryItemData(),
                 [EInventoryItemId.ELEVATOR_CALLER_BUTTON] = new InventoryItemData {IsDisposable = true},
                 [EInventoryItemId.ELEVATOR_CALLER_PANEL] = new InventoryItemData {IsDisposable = true}
             };
