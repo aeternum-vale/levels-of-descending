5bf7f3d5514bdaefec108549ecaddbe56ab67073
Artur Dedyulya <aeternum-vale@ya.ru>
2020-09-25 13:31:32 +0300

    update elevator caller panel hp and lp models and material

--- Assets/Scripts/InventoryModule/Inventory.cs
+++ Assets/Scripts/InventoryModule/Inventory.cs
@@ -23,10 +23,10 @@ namespace InventoryModule
                 [EInventoryItemId.LETTER] = new InventoryItemData(),
                 [EInventoryItemId.SCALPEL] = new InventoryItemData(),
                 [EInventoryItemId.E_PANEL_KEY] = new InventoryItemData(),
-                [EInventoryItemId.SCREWDRIVER] = new InventoryItemData(),
+                [EInventoryItemId.SCREWDRIVER] = new InventoryItemData() {IsInStock = true},
                 [EInventoryItemId.INSULATING_TAPE] = new InventoryItemData(),
-                [EInventoryItemId.ELEVATOR_CALLER_BUTTON] = new InventoryItemData {IsDisposable = true, IsInStock = true},
-                [EInventoryItemId.ELEVATOR_CALLER_PANEL] = new InventoryItemData {IsDisposable = true, IsInStock = true}
+                [EInventoryItemId.ELEVATOR_CALLER_BUTTON] = new InventoryItemData {IsDisposable = true},
+                [EInventoryItemId.ELEVATOR_CALLER_PANEL] = new InventoryItemData {IsDisposable = true}
             };
 
         private Image _backgroundImageComponent;
