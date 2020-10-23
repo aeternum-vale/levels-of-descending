2ddf0e99cd028846617f54eb4ce9035ea5c231a4
Artur Dedyulya <aeternum-vale@ya.ru>
2020-10-23 15:33:34 +0300

    fix garbage props settings

--- Assets/Scripts/FloorModule/PropsGenerator/GarbagePropsGenerator.cs
+++ Assets/Scripts/FloorModule/PropsGenerator/GarbagePropsGenerator.cs
@@ -5,23 +5,8 @@ namespace FloorModule.PropsGenerator
 {
     public class GarbagePropsGenerator : PropsGenerator
     {
-        private enum GarbageId : byte
-        {
-            GARBAGE_BAG = 0,
-            CAN,
-            CRUMPLED_PAPER,
-            BOTTLE,
-            CIGARETTE
-        }
-
-        [SerializeField] private GameObject garbageBagPrefab;
-        [SerializeField] private GameObject canPrefab;
-        [SerializeField] private GameObject crumpledPaperPrefab;
-        [SerializeField] private GameObject bottlePrefab;
-        [SerializeField] private GameObject cigarettePrefab;
-
-        private PropsRange _firstFloorRange =
-            new PropsRange()
+        private readonly PropsRange _firstFloorRange =
+            new PropsRange
             {
                 PositionY = new Vector2(-1.2f, -1.2f),
                 PositionX = new Vector2(-3.3f, -0.622f),
@@ -29,17 +14,42 @@ namespace FloorModule.PropsGenerator
                 RotationY = new Vector2(0, 359)
             };
 
+        private readonly PropsRange _secondFloorRange1 =
+            new PropsRange
+            {
+                PositionY = new Vector2(0.815f, 0.815f),
+                PositionX = new Vector2(1.808f, 3.561f),
+                PositionZ = new Vector2(1.663f, -0.899f),
+                RotationY = new Vector2(0, 359)
+            };
+
+        private readonly PropsRange _secondFloorRange2 =
+            new PropsRange
+            {
+                PositionY = new Vector2(0.815f, 0.815f),
+                PositionX = new Vector2(1.808f, 2.92f),
+                PositionZ = new Vector2(1.663f, -1.532f),
+                RotationY = new Vector2(0, 359)
+            };
+
+        [SerializeField] private GameObject bottlePrefab;
+        [SerializeField] private GameObject canPrefab;
+        [SerializeField] private GameObject cigarettePrefab;
+        [SerializeField] private GameObject crumpledPaperPrefab;
+
+        [SerializeField] private GameObject garbageBagPrefab;
+
         protected override void InitSchemes()
         {
             Schemes = new Dictionary<byte, PropsScheme>
             {
-                [(byte) GarbageId.GARBAGE_BAG] = new PropsScheme()
+                [(byte) GarbageId.GARBAGE_BAG] = new PropsScheme
                 {
                     Prefab = garbageBagPrefab,
                     AmountRange = new Vector2Int(0, 2),
                     Ranges = new[]
                     {
-                        new PropsRange()
+                        new PropsRange
                         {
                             PositionX = new Vector2(-3.162f, -2.595f),
                             PositionZ = new Vector2(1.657f, 1.657f),
@@ -47,50 +57,59 @@ namespace FloorModule.PropsGenerator
                         }
                     }
                 },
-                [(byte) GarbageId.BOTTLE] = new PropsScheme()
+                [(byte) GarbageId.BOTTLE] = new PropsScheme
                 {
                     Prefab = bottlePrefab,
-                    AmountRange = new Vector2Int(0, 5),
+                    AmountRange = new Vector2Int(0, 3),
                     Ranges = new[]
                     {
-                        new PropsRange()
+                        new PropsRange
                         {
                             PositionX = new Vector2(3.64f, 3.9f),
-                            PositionZ = new Vector2(0.39f, -0.34f),
+                            PositionZ = new Vector2(0.39f, -0.34f)
                         },
-                        new PropsRange()
+                        new PropsRange
                         {
                             PositionY = new Vector2(0.817f, 0.817f),
                             PositionX = new Vector2(3.559f, 3.155f),
-                            PositionZ = new Vector2(1.679f, -1.098f),
+                            PositionZ = new Vector2(1.679f, -1.098f)
                         }
                     }
                 },
-                [(byte) GarbageId.CIGARETTE] = new PropsScheme()
+                [(byte) GarbageId.CIGARETTE] = new PropsScheme
                 {
                     Prefab = cigarettePrefab,
-                    AmountRange = new Vector2Int(2, 20),
+                    AmountRange = new Vector2Int(2, 10),
                     Ranges = new[]
                     {
-                        _firstFloorRange
+                        _firstFloorRange,
+                        _firstFloorRange,
+                        _secondFloorRange1,
+                        _secondFloorRange2
                     }
                 },
-                [(byte) GarbageId.CRUMPLED_PAPER] = new PropsScheme()
+                [(byte) GarbageId.CRUMPLED_PAPER] = new PropsScheme
                 {
                     Prefab = crumpledPaperPrefab,
-                    AmountRange = new Vector2Int(0, 2),
+                    AmountRange = new Vector2Int(1, 2),
                     Ranges = new[]
                     {
-                        _firstFloorRange
+                        _firstFloorRange,
+                        _firstFloorRange,
+                        _secondFloorRange1,
+                        _secondFloorRange2
                     }
                 },
-                [(byte) GarbageId.CAN] = new PropsScheme()
+                [(byte) GarbageId.CAN] = new PropsScheme
                 {
                     Prefab = canPrefab,
-                    AmountRange = new Vector2Int(0, 2),
+                    AmountRange = new Vector2Int(0, 1),
                     Ranges = new[]
                     {
-                        _firstFloorRange
+                        _firstFloorRange,
+                        _firstFloorRange,
+                        _secondFloorRange1,
+                        _secondFloorRange2
                     }
                 }
             };
@@ -99,6 +118,15 @@ namespace FloorModule.PropsGenerator
         protected override void ApplyAdditionalSettingsToProp(GameObject currentInstance, GameObject prefab,
             PropsRange range)
         {
+        }
+
+        private enum GarbageId : byte
+        {
+            GARBAGE_BAG = 0,
+            CAN,
+            CRUMPLED_PAPER,
+            BOTTLE,
+            CIGARETTE
         }
     }
 }
